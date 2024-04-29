228160a0b8e228c42ca97fe45e1b111baf516bf1
Daniel Kukula <daniel.kuku@gmail.com>
2024-04-29 21:24:06 +0100

    refactor to more universal model

--- lib/tf_idf.ex
+++ lib/tf_idf.ex
@@ -1,76 +1,81 @@
 defmodule TfIdf do
   alias :math, as: Math
 
-  def build_tf_idf(documents, opts) do
-    word_hashmap = generate_word_hashmap(documents, opts[:lowercase])
+  defmodule Doc do
+    defstruct [:last_modified, tf: %{}, count: 0]
+  end
+
+  defmodule Model do
+    defstruct docs: [], count: 0, idf: %{}
+  end
+
+  def build_model(docs) do
+    docs
+    |> generate_docs_tf()
+    |> generate_idf()
+  end
+
+  def calculate_relevance(model, query) do
+    model.docs
+    |> Enum.reduce([], fn {doc_name, %{tf: tf, count: total_count}}, acc ->
+      score =
+        query
+        |> String.downcase()
+        |> String.split(~r/[^a-zA-Z]/)
+        |> Enum.map(fn token ->
+          idf = Map.get(model.idf, token, 0)
+          token_count = Map.get(tf, token, 0)
+          tf = token_count / total_count
+          tf * idf
+        end)
+        |> Enum.sum()
+
+      if score == 0 do
+        acc
+      else
+        [{doc_name, score} | acc]
+      end
+    end)
+    |> Enum.sort_by(fn {_, score} -> score end, :desc)
+  end
+
+  def calculate_token_value(model, token) do
+    idf = Map.get(model.idf, token, 0)
+
+    model.docs
+    |> Enum.map(fn {doc_name, %{tf: tf, count: total_count}} ->
+      token_count = Map.get(tf, token, 0)
+      tf = token_count / total_count
+      {doc_name, tf * idf}
+    end)
+  end
+
+  def generate_docs_tf(docs) do
+    Enum.map(docs, fn doc ->
+      words =
+        doc
+        |> String.downcase()
+        |> String.split(~r/[^a-zA-Z]/)
+
+      n = length(words)
+
+      tf =
+        words
+        |> Enum.frequencies()
+
+      {doc, %TfIdf.Doc{tf: tf, last_modified: DateTime.utc_now(), count: n}}
+    end)
+  end
+
+  def generate_idf(docs) do
+    n = length(docs)
 
     idf =
-      documents
-      |> generate_unique_word_hashmap(opts[:lowercase])
-      |> calculate_idf(length(documents))
+      docs
+      |> Enum.flat_map(&Map.keys(elem(&1, 1).tf))
+      |> Enum.frequencies()
+      |> Map.new(fn {t, c} -> {t, Math.log((n + 1) / (c + 1))} end)
 
-    word_hashmap
-    |> calculate_tf()
-    |> calculate_tf_idf(idf)
-    |> l2_normalize()
-  end
-
-  def generate_word_hashmap(documents, _lowercase = true) do
-    documents
-    |> Enum.map(&String.downcase/1)
-    |> Enum.flat_map(fn document -> String.split(document, ~r/\s+/) end)
-    |> Enum.frequencies()
-  end
-
-  def generate_word_hashmap(documents, _) do
-    documents
-    |> Enum.flat_map(fn document -> String.split(document, ~r/\s+/) end)
-    |> Enum.frequencies()
-  end
-
-  def generate_unique_word_hashmap(documents, _lowercase = true) do
-    documents
-    |> Enum.flat_map(fn document ->
-      for word <- String.split(document, ~r/\s+/), uniq: true, do: String.downcase(word)
-    end)
-    |> Enum.frequencies()
-  end
-
-  def generate_unique_word_hashmap(documents, _) do
-    documents
-    |> Enum.flat_map(fn document ->
-      for word <- String.split(document, ~r/\s+/), uniq: true, do: word
-    end)
-    |> Enum.frequencies()
-  end
-
-  def calculate_tf(freq_map) do
-    total_words = freq_map |> Map.values() |> Enum.sum()
-    Map.new(freq_map, fn {word, count} -> {word, count / total_words} end)
-  end
-
-  def calculate_idf(freq_map, docs_count) do
-    Map.new(freq_map, fn {word, word_count} ->
-      documents_with_term = (docs_count + 1) / (word_count + 1)
-
-      {word, Math.log(documents_with_term) + 1}
-    end)
-  end
-
-  def calculate_tf_idf(tf, idf) do
-    Map.new(tf, fn {word, count} ->
-      {word, count * Map.get(idf, word, 0)}
-    end)
-  end
-
-  def l2_normalize(tf_idf) do
-    l2_norm =
-      tf_idf
-      |> Map.values()
-      |> Enum.map(&Math.pow(&1, 2))
-      |> Enum.sum()
-      |> Math.sqrt()
-
-    Map.new(tf_idf, fn {word, value} -> {word, value / l2_norm} end)
+    %TfIdf.Model{docs: docs, count: n, idf: idf}
   end
 end
--- test/tf_idf_test.exs
+++ test/tf_idf_test.exs
@@ -9,14 +9,71 @@ defmodule TfIdfTest do
            This is a third test document.
            """
            |> String.split("\n", trim: true)
-           |> TfIdf.build_tf_idf(lowercase: true) == %{
-             "a" => 0.3703126778946051,
-             "another" => 0.24345833490911345,
-             "is" => 0.43137124351221257,
-             "test" => 0.43137124351221257,
-             "third" => 0.24345833490911345,
-             "this" => 0.43137124351221257,
-             "document." => 0.43137124351221257
+           |> TfIdf.build_model() == %TfIdf.Model{
+             count: 3,
+             docs: [
+               {"This is a test document.",
+                %TfIdf.Doc{
+                  last_modified: ~U[2024-04-29 19:38:42.878419Z],
+                  tf: %{"" => 1, "a" => 1, "document" => 1, "is" => 1, "test" => 1, "this" => 1},
+                  count: 6
+                }},
+               {"This is another test document.",
+                %TfIdf.Doc{
+                  last_modified: ~U[2024-04-29 19:38:42.879522Z],
+                  tf: %{
+                    "" => 1,
+                    "another" => 1,
+                    "document" => 1,
+                    "is" => 1,
+                    "test" => 1,
+                    "this" => 1
+                  },
+                  count: 6
+                }},
+               {"This is a third test document.",
+                %TfIdf.Doc{
+                  last_modified: ~U[2024-04-29 19:38:42.879533Z],
+                  tf: %{
+                    "" => 1,
+                    "a" => 1,
+                    "document" => 1,
+                    "is" => 1,
+                    "test" => 1,
+                    "third" => 1,
+                    "this" => 1
+                  },
+                  count: 7
+                }}
+             ],
+             idf: %{
+               "" => 0.0,
+               "a" => 0.28768207245178085,
+               "another" => 0.6931471805599453,
+               "document" => 0.0,
+               "is" => 0.0,
+               "test" => 0.0,
+               "third" => 0.6931471805599453,
+               "this" => 0.0
+             }
            }
+  end
+
+  test "wikipedia example" do
+    model =
+      """
+      This is a a sample
+      This is another another example example example
+      """
+      |> String.split("\n", trim: true)
+      |> TfIdf.build_model()
+
+    assert TfIdf.calculate_relevance(model, "example") ==
+             [{"This is another another example example example", 0.17377076061778474}]
+
+    assert TfIdf.calculate_relevance(model, "sample") ==
+             [{"This is a a sample", 0.08109302162163289}]
+
+    assert TfIdf.calculate_relevance(model, "this") == []
   end
 end
